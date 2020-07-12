2554d3206c28b48d041b5ad8f8a52dd31e858b2e
sungjae.jung1031 <sungjae.jung1031@gmail.com>
2020-07-12 23:19:01 +0900

    update dash board (stil need an update for pie chart)

--- twitter_sentiment_analysis_app.py
+++ twitter_sentiment_analysis_app.py
@@ -3,34 +3,106 @@ from dash.dependencies import Output, Input
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly
+import plotly.graph_objects as go
 import random
-import plotly.graph_objs as go
 from collections import deque
 import sqlite3
 import pandas as pd
+from cache import cache
+import time
 
 # popular topics: google, olympics, trump, gun, usa
 
+color_palette = {
+    'orange': '#E95420',
+    'white': '#FFFFFF',
+    'black': '#000000',
+    'aubergine_light': '#77216F',
+    'aubergine_mid': '#5E2750',
+    'aubergine_dark': '#2C001E',
+    'aubergine_canonical': '#772953',
+    'grey_warm': '#AEA79F',
+    'grey_cool': '#333333',
+    'grey_text': '#111111',
+    'red': '#FF0000',
+    'blue': '#0000FF'
+}
+
+
 app = dash.Dash(__name__)
 app.layout = html.Div(
-    [html.H2('Live Twitter Sentiment'),
-        dcc.Input(id='sentiment_term', value='Trump', type='text'),
-        dcc.Graph(id='live-graph', animate=False),
+    [html.Div(className='container-fluid', children=[html.H2('Live Twitter Sentiment', style={'color': color_palette['black']}),
+                                                     html.H4('Search:', style={
+                                                         'color': color_palette['black']}),
+                                                     dcc.Input(id='sentiment_term', value='twitter', type='text', style={
+                                                               'color': color_palette['black']}),
+                                                     ],
+              style={'width': '98%', 'margin-top': 50, 'margin-left': 10, 'margin-right': 10, 'max-width': 50000}),
+
+        # html.Div(className='row', children=[html.Div(id='related-sentiment', children=html.Button('Loading related terms...', id='related_term_button'), className='col s12 m6 l6', style={"word-wrap": "break-word"}),
+        #                                     html.Div(id='recent-trending', className='col s12 m6 l6', style={"word-wrap": "break-word"})]),
+
+        html.Div(className='row', style={'display': 'flex'}, children=[html.Div(
+            dcc.Graph(id='live-graph', animate=False), className='col s12 m6 l6'), html.Div(
+            dcc.Graph(id='world-map', animate=False), className='col s12 m6 l6')]),
+
+        # html.Div(className='row', children=[html.Div(
+        #     id="recent-tweets-table", className='col s12 m6 l6')]),
+
+        html.Div(className='row', style={'display': 'flex'}, children=[
+                 html.Div(id="recent-tweets-table", className='col s12 m6 l6'),
+                 html.Div(dcc.Graph(id='sentiment-pie', animate=False), className='col s12 m6 l6')]),
+
+        # html.Div(className='row', children=[html.Div(dcc.Graph(id='live-graph', animate=False), className='col s12 m6 l6'),
+        #                                     html.Div(dcc.Graph(id='historical-graph', animate=False), className='col s12 m6 l6')]),
+
+        # html.Div(className='row', children=[html.Div(
+        #     id="recent-tweets-table", className='col s12 m6 l6')]),
+
+        # html.Div(className='row', children=[html.Div(id="recent-tweets-table", className='col s12 m6 l6'),
+        #                                     html.Div(dcc.Graph(id='sentiment-pie', animate=False), className='col s12 m6 l6'), ]),
+
+     dcc.Interval(
+        id='live-graph-update',
+        interval=1*1000
+    ),
+
         dcc.Interval(
-            id='graph-update',
+        id='recent-tweets-table-update',
+        interval=2*1000
+    ),
+
+        dcc.Interval(
+            id='world-map-update',
             interval=1*1000
     ),
-    ]
+        #     dcc.Interval(
+        #         id='historical-update',
+        #         interval=60*1000
+        # ),
+
+        #     dcc.Interval(
+        #         id='related-update',
+        #         interval=30*1000
+        # ),
+
+
+
+        dcc.Interval(
+        id='sentiment-pie-update',
+        interval=60*1000
+    ),
+
+    ], style={'backgroundColor': color_palette['white'], 'margin-top': '-30px', 'height': '2000px', },
 )
 
 
-@app.callback(Output('live-graph', 'figure'),
-              [Input(component_id='sentiment_term', component_property='value'),
-               Input('graph-update', 'n_intervals')])
+@ app.callback(Output('live-graph', 'figure'),
+               [Input(component_id='sentiment_term', component_property='value'),
+                Input('live-graph-update', 'n_intervals')])
 def update_graph_scatter(sentiment_term, datac):
     try:
-        conn = sqlite3.connect('twitter_live.db')
-        c = conn.cursor()
+        conn = sqlite3.connect('twitter_live_bak.db')
         df = pd.read_sql(
             "SELECT * FROM sentiment WHERE tweet LIKE ? ORDER BY unix DESC LIMIT 1000", conn, params=('%' + sentiment_term + '%',))
         df.sort_values('unix', inplace=True)
@@ -60,6 +132,162 @@ def update_graph_scatter(sentiment_term, datac):
         with open('errors.txt', 'a') as f:
             f.write(str(e))
             f.write('\n')
+
+
+POS_NEG_NEUT = 0.1
+
+
+def quick_color(s):
+    # except return bg as app_colors['background']
+    if s >= POS_NEG_NEUT:
+        # positive
+        return color_palette['aubergine_light']
+    elif s <= -POS_NEG_NEUT:
+        # negative:
+        return color_palette['orange']
+
+    else:
+        return color_palette['grey_cool']
+
+
+def generate_table(df, max_rows=10):
+    return html.Table(className="responsive-table",
+                      children=[
+                          html.Thead(
+                              html.Tr(
+                                  children=[
+                                      html.Th(col.title()) for col in df.columns.values],
+                                  style={'color': color_palette['grey_text']}
+                              )
+                          ),
+                          html.Tbody(
+                              [
+
+                                  html.Tr(
+                                      children=[
+                                          html.Td(data) for data in d
+                                      ], style={'color': color_palette['white'],
+                                                'background-color':quick_color(d[2])}
+                                  )
+                                  for d in df.values.tolist()])
+                      ]
+                      )
+
+
+@ app.callback(Output('recent-tweets-table', 'children'),
+               [Input(component_id='sentiment_term', component_property='value'),
+                Input('recent-tweets-table-update', 'n_intervals')])
+def update_recent_tweets(sentiment_term, datac):
+    try:
+        conn = sqlite3.connect('twitter_live_bak.db')
+        if sentiment_term:
+            df = pd.read_sql("SELECT sentiment.* FROM sentiment_fts fts LEFT JOIN sentiment ON fts.rowid = sentiment.id WHERE fts.sentiment_fts MATCH ? ORDER BY fts.rowid DESC LIMIT 10",
+                             conn, params=(sentiment_term+'*',))
+        else:
+            df = pd.read_sql(
+                "SELECT * FROM sentiment ORDER BY id DESC, unix DESC LIMIT 10", conn)
+
+        df['date'] = pd.to_datetime(df['unix'], unit='ms')
+
+        df = df.drop(['unix', 'id'], axis=1)
+        df = df[['date', 'tweet', 'sentiment']]
+
+        return generate_table(df, max_rows=10)
+    except Exception as e:
+        with open('errors.txt', 'a') as f:
+            f.write(str(e))
+            f.write('\n')
+
+
+@ app.callback(Output('world-map', 'figure'),
+               [Input(component_id='sentiment_term', component_property='value'),
+                Input('world-map-update', 'n_intervals')])
+def update_map(sentiment_term, datac):
+
+    df = pd.read_csv(
+        '2014_world_gdp_with_codes.csv')
+
+    try:
+        fig = go.Figure(data=go.Choropleth(
+            locations=df['CODE'],
+            z=df['GDP (BILLIONS)'],
+            text=df['COUNTRY'],
+            colorscale='Rainbow',  # Rainbow, Blues, Reds
+            autocolorscale=False,
+            reversescale=True,
+            marker_line_color='darkgray',
+            marker_line_width=0.5,
+            colorbar_tickprefix='%',
+            colorbar_title='Positive<br>Sentiment<br>Intensity',
+        ))
+
+        fig.update_layout(
+            title_text=sentiment_term,
+            geo=dict(
+                showframe=False,
+                showcoastlines=False,
+                projection_type='equirectangular'
+            ),
+            annotations=[dict(
+                x=0.55,
+                y=0.1,
+                xref='paper',
+                yref='paper',
+                text='Source: <a href="https://www.cia.gov/library/publications/the-world-factbook/fields/2195.html">\
+                    CIA World Factbook</a>',
+                showarrow=False
+            )],
+        )
+
+    except:
+        pass
+
+    return (fig)
+
+
+@app.callback(Output('sentiment-pie', 'figure'),
+              [Input(component_id='sentiment_term', component_property='value'),
+               Input('sentiment-pie-update', 'n_intervals')])
+def update_pie_chart(sentiment_term, datac):
+
+    # get data from cache
+    for _ in range(100):
+        sentiment_pie_dict = cache.get('sentiment_shares', sentiment_term)
+        if sentiment_pie_dict:
+            break
+        time.sleep(0.1)
+
+    if not sentiment_pie_dict:
+        return None
+
+    labels = ['Positive', 'Negative']
+
+    try:
+        pos = sentiment_pie_dict[1]
+    except:
+        pos = 0
+
+    try:
+        neg = sentiment_pie_dict[-1]
+    except:
+        neg = 0
+
+    values = [pos, neg]
+    colors = [color_palette['aubergine_light'], color_palette['orange']]
+
+    trace = go.Pie(labels=labels, values=values,
+                   hoverinfo='label+percent', textinfo='value',
+                   textfont=dict(size=20, color=color_palette['black']),
+                   marker=dict(colors=colors,
+                               line=dict(color=color_palette['black'], width=2)))
+
+    return {"data": [trace], 'layout': go.Layout(
+        title='Positive vs Negative sentiment for "{}" (longer-term)'.format(
+            sentiment_term),
+        font={'color': color_palette['black']},
+        plot_bgcolor=color_palette['black'],
+        paper_bgcolor=color_palette['black'],
+        showlegend=True)}
 
 
 if __name__ == '__main__':
--- twitter_table_creator.py
+++ twitter_table_creator.py
@@ -1,11 +1,26 @@
-from tweepy import Stream
-from tweepy import OAuthHandler
-from tweepy.streaming import StreamListener
-import json
-import sqlite3
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-from unidecode import unidecode
+# set chdir to current dir
+from textblob import TextBlob
+import itertools
+import pickle
+import string
+from collections import Counter
+import regex as re
+from config import stop_words
+import pandas as pd
+from threading import Lock, Timer
 import time
+from unidecode import unidecode
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import sqlite3
+import json
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import os
+import sys
+sys.path.insert(0, os.path.realpath(os.path.dirname(__file__)))
+os.chdir(os.path.realpath(os.path.dirname(__file__)))
+
 
 analyzer = SentimentIntensityAnalyzer()
 
@@ -14,85 +29,256 @@ ckey = "H1uCbkmrRHCYfZJdss4sOiees"
 csecret = "kRn2CSdozYb1yqLEkSdZ59XBjIAI9SV4EfJ9eHUER4TXe4jB3n"
 atoken = "991302229972860930-6pcAUmE1tqwhvkn8ovCudchJLnCqk3x"
 asecret = "fsmNVS2bcdEirXBSRZ45LdaA5Faq0XLeFvsuc4ofKLz5j"
-
-conn = sqlite3.connect(
-    '/home/sungjae/Documents/git/twitter-sentiment-analysis/twitter_live.db')
+# isolation lever disables automatic transactions,
+# we are disabling thread check as we are creating connection here, but we'll be inserting from a separate thread (no need for serialization)
+conn = sqlite3.connect('/home/sungjae/Documents/git/twitter-sentiment-analysis/twitter_live_bak.db',
+                       isolation_level=None, check_same_thread=False)
 c = conn.cursor()
 
 
 def create_table():
     try:
-        # (unix, tweet, sentiment, loc_pnt_long, loc_pnt_lat, lang, friends_cnt, followers_cnt, retweet_cnt)
+
+        # http://www.sqlite.org/pragma.html#pragma_journal_mode
+        # for us - it allows concurrent write and reads
+        c.execute("PRAGMA journal_mode=wal")
+        c.execute("PRAGMA wal_checkpoint=TRUNCATE")
+        #c.execute("PRAGMA journal_mode=PERSIST")
+
+        # changed unix to INTEGER (it is integer, sqlite can use up to 8-byte long integers)
+        c.execute("CREATE TABLE IF NOT EXISTS sentiment(id INTEGER PRIMARY KEY AUTOINCREMENT, unix INTEGER, tweet TEXT, sentiment REAL)")
+        # key-value table for random stuff
         c.execute(
-            "CREATE TABLE IF NOT EXISTS sentiment(unix REAL, tweet TEXT, sentiment REAL, sentiment_pos REAL, sentiment_neg REAL, sentiment_neu REAL, loc_pnt_long REAL, loc_pnt_lat REAL, lang TEXT, friends_cnt INT, followers_cnt INT, retweet_cnt INT)")
-        c.execute("CREATE INDEX fast_unix ON sentiment(unix)")
-        c.execute("CREATE INDEX fast_tweet ON sentiment(tweet)")
-        c.execute("CREATE INDEX fast_sentiment ON sentiment(sentiment)")
-        c.execute("CREATE INDEX fast_sentiment_pos ON sentiment(sentiment_pos)")
-        c.execute("CREATE INDEX fast_sentiment_neg ON sentiment(sentiment_neg)")
-        c.execute("CREATE INDEX fast_sentiment_neu ON sentiment(sentiment_neu)")
-        c.execute("CREATE INDEX fast_loc_pnt_long ON sentiment(loc_pnt_long)")
-        c.execute("CREATE INDEX fast_loc_pnt_lat ON sentiment(loc_pnt_lat)")
-        c.execute("CREATE INDEX fast_lang ON sentiment(lang)")
-        c.execute("CREATE INDEX fast_friends_cnt ON sentiment(friends_cnt)")
-        c.execute("CREATE INDEX fast_followers_cnt ON sentiment(follwers_cnt)")
-        c.execute("CREATE INDEX fast_retweet_cnt ON sentiment(retweet_cnt)")
-        conn.commit()
+            "CREATE TABLE IF NOT EXISTS misc(key TEXT PRIMARY KEY, value TEXT)")
+        # id on index, both as DESC (as you are sorting in DESC order)
+        c.execute("CREATE INDEX id_unix ON sentiment (id DESC, unix DESC)")
+        # out full-text search table, i choosed creating data from external (content) table - sentiment
+        # instead of directly inserting to that table, as we are saving more data than just text
+        # https://sqlite.org/fts5.html - 4.4.2
+        c.execute("CREATE VIRTUAL TABLE sentiment_fts USING fts5(tweet, content=sentiment, content_rowid=id, prefix=1, prefix=2, prefix=3)")
+        # that trigger will automagically update out table when row is interted
+        # (requires additional triggers on update and delete)
+        c.execute("""
+            CREATE TRIGGER sentiment_insert AFTER INSERT ON sentiment BEGIN
+                INSERT INTO sentiment_fts(rowid, tweet) VALUES (new.id, new.tweet);
+            END
+        """)
     except Exception as e:
         print(str(e))
 
 
 create_table()
 
+# create lock
+lock = Lock()
+
 
 class listener(StreamListener):
 
+    data = []
+    lock = None
+
+    def __init__(self, lock):
+
+        # create lock
+        self.lock = lock
+
+        # init timer for database save
+        self.save_in_database()
+
+        # call __inint__ of super class
+        super().__init__()
+
+    def save_in_database(self):
+
+        # set a timer (1 second)
+        Timer(1, self.save_in_database).start()
+
+        # with lock, if there's data, save in transaction using one bulk query
+        with self.lock:
+            if len(self.data):
+                c.execute('BEGIN TRANSACTION')
+                try:
+                    c.executemany(
+                        "INSERT INTO sentiment (unix, tweet, sentiment) VALUES (?, ?, ?)", self.data)
+                except:
+                    pass
+                c.execute('COMMIT')
+
+                self.data = []
+
     def on_data(self, data):
         try:
+            # print('data')
             data = json.loads(data)
-
+            # there are records like that:
+            # {'limit': {'track': 14667, 'timestamp_ms': '1520216832822'}}
+            if 'truncated' not in data:
+                # print(data)
+                return True
+            if data['truncated']:
+                tweet = unidecode(data['extended_tweet']['full_text'])
+            else:
+                tweet = unidecode(data['text'])
             time_ms = data['timestamp_ms']
-            tweet = unidecode(data['text'])
             vs = analyzer.polarity_scores(tweet)
             sentiment = vs['compound']
-            sentiment_pos = vs['pos']
-            sentiment_neg = vs['neg']
-            sentiment_neu = vs['neu']
-            coordinate_obj = data['coordinates']
-            loc_pnt_long = 1024.0
-            loc_pnt_lat = 1024.0
+            #print(time_ms, tweet, sentiment)
 
-            if coordinate_obj != None:
-                loc_pnt_long, loc_pnt_lat = data['coordinates']['coordinates']
+            # append to data list (to be saved every 1 second)
+            with self.lock:
+                self.data.append((time_ms, tweet, sentiment))
 
-            lang = data['lang']
-            friends_cnt = data['user']['friends_count']
-            followers_cnt = data['user']['followers_count']
-            retweet_cnt = data['retweet_count']
-
-            print(time_ms, tweet, sentiment, loc_pnt_long, loc_pnt_lat, lang,
-                  friends_cnt, retweet_cnt)
-
-            c.execute("INSERT INTO sentiment (unix, tweet, sentiment, sentiment_pos, sentiment_neg, sentiment_neu, loc_pnt_long, loc_pnt_lat, lang, friends_cnt, followers_cnt, retweet_cnt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                      (time_ms, tweet, sentiment, sentiment_pos, sentiment_neg, sentiment_neu, loc_pnt_long, loc_pnt_lat, lang, friends_cnt, followers_cnt, retweet_cnt))
-
-            conn.commit()
         except KeyError as e:
+            # print(data)
             print(str(e))
-
         return True
 
     def on_error(self, status):
         print(status)
 
 
+# make a counter with blacklist words and empty word with some big value - we'll use it later to filter counter
+stop_words.append('')
+blacklist_counter = Counter(dict(zip(stop_words, [1000000] * len(stop_words))))
+
+# complie a regex for split operations (punctuation list, plus space and new line)
+punctuation = [str(i) for i in string.punctuation]
+split_regex = re.compile("[ \n" + re.escape("".join(punctuation)) + ']')
+
+
+def map_nouns(col):
+    return [word[0] for word in TextBlob(col).tags if word[1] == u'NNP']
+
+# generate "trending"
+
+
+def generate_trending():
+
+    try:
+        # select last 10k tweets
+        df = pd.read_sql(
+            "SELECT * FROM sentiment ORDER BY id DESC, unix DESC LIMIT 10000", conn)
+        df['nouns'] = list(map(map_nouns, df['tweet']))
+
+        # make tokens
+        tokens = split_regex.split(' '.join(
+            list(itertools.chain.from_iterable(df['nouns'].values.tolist()))).lower())
+        # clean and get top 10
+        trending = (Counter(tokens) - blacklist_counter).most_common(10)
+
+        # get sentiments
+        trending_with_sentiment = {}
+        for term, count in trending:
+            df = pd.read_sql(
+                "SELECT sentiment.* FROM  sentiment_fts fts LEFT JOIN sentiment ON fts.rowid = sentiment.id WHERE fts.sentiment_fts MATCH ? ORDER BY fts.rowid DESC LIMIT 1000", conn, params=(term,))
+            trending_with_sentiment[term] = [df['sentiment'].mean(), count]
+
+        # save in a database
+        with lock:
+            c.execute('BEGIN TRANSACTION')
+            try:
+                c.execute("REPLACE INTO misc (key, value) VALUES ('trending', ?)",
+                          (pickle.dumps(trending_with_sentiment),))
+            except:
+                pass
+            c.execute('COMMIT')
+
+    except Exception as e:
+        with open('errors.txt', 'a') as f:
+            f.write(str(e))
+            f.write('\n')
+    finally:
+        Timer(5, generate_trending).start()
+
+
+Timer(1, generate_trending).start()
+
 while True:
 
     try:
         auth = OAuthHandler(ckey, csecret)
         auth.set_access_token(atoken, asecret)
-        twitterStream = Stream(auth, listener())
+        twitterStream = Stream(auth, listener(lock))
         twitterStream.filter(track=["a", "e", "i", "o", "u"])
     except Exception as e:
         print(str(e))
         time.sleep(5)
+
+
+# def create_table():
+#     try:
+#         # (unix, tweet, sentiment, loc_pnt_long, loc_pnt_lat, lang, friends_cnt, followers_cnt, retweet_cnt)
+#         c.execute(
+#             "CREATE TABLE IF NOT EXISTS sentiment(unix REAL, tweet TEXT, sentiment REAL, sentiment_pos REAL, sentiment_neg REAL, sentiment_neu REAL, loc_pnt_long REAL, loc_pnt_lat REAL, lang TEXT, friends_cnt INT, followers_cnt INT, retweet_cnt INT)")
+#         c.execute("CREATE INDEX fast_unix ON sentiment(unix)")
+#         c.execute("CREATE INDEX fast_tweet ON sentiment(tweet)")
+#         c.execute("CREATE INDEX fast_sentiment ON sentiment(sentiment)")
+#         c.execute("CREATE INDEX fast_sentiment_pos ON sentiment(sentiment_pos)")
+#         c.execute("CREATE INDEX fast_sentiment_neg ON sentiment(sentiment_neg)")
+#         c.execute("CREATE INDEX fast_sentiment_neu ON sentiment(sentiment_neu)")
+#         c.execute("CREATE INDEX fast_loc_pnt_long ON sentiment(loc_pnt_long)")
+#         c.execute("CREATE INDEX fast_loc_pnt_lat ON sentiment(loc_pnt_lat)")
+#         c.execute("CREATE INDEX fast_lang ON sentiment(lang)")
+#         c.execute("CREATE INDEX fast_friends_cnt ON sentiment(friends_cnt)")
+#         c.execute("CREATE INDEX fast_followers_cnt ON sentiment(follwers_cnt)")
+#         c.execute("CREATE INDEX fast_retweet_cnt ON sentiment(retweet_cnt)")
+#         conn.commit()
+#     except Exception as e:
+#         print(str(e))
+
+
+# create_table()
+
+
+# class listener(StreamListener):
+
+#     def on_data(self, data):
+#         try:
+#             data = json.loads(data)
+
+#             time_ms = data['timestamp_ms']
+#             tweet = unidecode(data['text'])
+#             vs = analyzer.polarity_scores(tweet)
+#             sentiment = vs['compound']
+#             sentiment_pos = vs['pos']
+#             sentiment_neg = vs['neg']
+#             sentiment_neu = vs['neu']
+#             coordinate_obj = data['coordinates']
+#             loc_pnt_long = 1024.0
+#             loc_pnt_lat = 1024.0
+
+#             if coordinate_obj != None:
+#                 loc_pnt_long, loc_pnt_lat = data['coordinates']['coordinates']
+
+#             lang = data['lang']
+#             friends_cnt = data['user']['friends_count']
+#             followers_cnt = data['user']['followers_count']
+#             retweet_cnt = data['retweet_count']
+
+#             print(time_ms, tweet, sentiment, loc_pnt_long, loc_pnt_lat, lang,
+#                   friends_cnt, retweet_cnt)
+
+#             c.execute("INSERT INTO sentiment (unix, tweet, sentiment, sentiment_pos, sentiment_neg, sentiment_neu, loc_pnt_long, loc_pnt_lat, lang, friends_cnt, followers_cnt, retweet_cnt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+#                       (time_ms, tweet, sentiment, sentiment_pos, sentiment_neg, sentiment_neu, loc_pnt_long, loc_pnt_lat, lang, friends_cnt, followers_cnt, retweet_cnt))
+
+#             conn.commit()
+#         except KeyError as e:
+#             print(str(e))
+
+#         return True
+
+#     def on_error(self, status):
+#         print(status)
+
+
+# while True:
+
+#     try:
+#         auth = OAuthHandler(ckey, csecret)
+#         auth.set_access_token(atoken, asecret)
+#         twitterStream = Stream(auth, listener())
+#         twitterStream.filter(track=["a", "e", "i", "o", "u"])
+#     except Exception as e:
+#         print(str(e))
+#         time.sleep(5)
